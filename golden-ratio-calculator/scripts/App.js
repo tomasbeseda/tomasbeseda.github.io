dd5c3f1e3dee8f2a7b6c3c7e0e1cc5106645e512
tomas.beseda@etnetera.cz <tomas.beseda@etnetera.cz>
2016-09-25 23:03:02 +0200

    Added: error messages, empty state

--- golden-ratio-calculator/scripts/App.js
+++ golden-ratio-calculator/scripts/App.js
@@ -12,25 +12,47 @@ var App = {
             App.append('decreasing', decrease);
         }
     },
-    append: function(element, content, clear) {
+    append: function(element, content, clear, error) {
         if (clear && element) {
             document.getElementById(element).innerHTML = ' ';
         }
         if (element && content) {
             document.getElementById(element).innerHTML += '<div class="list__item">' + content + '</div>';
         }
+        if (element && content && error) {
+            document.getElementById(element).innerHTML = '<div class="error-message error-message--red">' + content + '</div>';
+        }
     },
     getValue: function(element) {
         return document.getElementById(element).value;
     },
+    error: function(number, iteration) {
+        var selector = 'error',
+            message;
+        if (!number && !iteration) {
+            message = 'Form cannot be empty';
+        } else if (!number) {
+            message = 'Base number cannot be empty';
+        } else {
+            message = 'Iteration cannot be empty';
+        }
+        App.append(selector, message, null, true);
+    },
+    clearAppends: function() {
+        App.append('increasing', null, true);
+        App.append('decreasing', null, true);
+        App.append('error', null, true);
+        App.append('empty-state', null, true);
+    },
     init: function() {
         document.getElementById('count').addEventListener('click', function() {
-            App.append('increasing', null, true);
-            App.append('decreasing', null, true);
+            App.clearAppends();
             var number = App.getValue('number'),
                 iteration = App.getValue('iteration');
             if (number && iteration) {
                 App.magic(number, iteration);
+            } else {
+                App.error(number, iteration);
             }
         });
     }
